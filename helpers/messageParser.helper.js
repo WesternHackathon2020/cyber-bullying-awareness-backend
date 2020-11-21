b7641410f41cefd5f329d0bfe7b3153c681c2857
Will Hutchinson <willhutchinson2.0@gmail.com>
2020-11-21 10:22:50 -0500

    added csv parser

---
+++ helpers/messageParser.helper.js
@@ -0,0 +1,30 @@
+const csv = require('csv-parser')
+const fs = require('fs')
+
+const csvArrayToMap = (array) => {
+    const senderMap = {}
+    for(let i = 0; i < array.length; i++){
+        let message = array[i]
+        message.MESSAGE = message.MESSAGE.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+        message.MESSAGE += '.'
+        senderMap[i] = array[i]
+    }
+    return senderMap
+}
+
+const csvToMap = async (filename) => {
+
+    const results = [];
+
+    return new Promise((resolve, reject) => {
+        fs.createReadStream(`../temp/${filename}`)
+            .pipe(csv())
+            .on('data', (data) => results.push(data))
+            .on('end', async () => {
+                resolve(await csvArrayToMap(results))
+            })
+            .on('error', () => {
+                reject()
+            });
+    });
+}
