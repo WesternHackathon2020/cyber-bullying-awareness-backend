497cac12b5b32cf4612cf0ccf4c0ac66dc792290
isaiahb <isaiahballah@gmail.com>
2020-11-22 02:27:02 -0500

    please cors don't block me

--- index.js
+++ index.js
@@ -3,11 +3,29 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
+
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+    origin: [
+      'http://localhost:9080',
+      'https://localhost:3000',
+      'http://192.168.1.167:9080',
+      'http://192.168.1.167:3000',
+      'http://192.168.0.121:3000',
+      'http://192.168.0.121:9080',
+  
+      'http://localhost:8000',
+      'http://192.168.0.121:8000',
+      '*',
+    ],
+    credentials: true,
+    exposedHeaders: ['set-cookie']
+  }));
 
+  
 const mongoURL = process.env.DB_URL;
 mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true }).catch((err)=>{
     console.log(err);
